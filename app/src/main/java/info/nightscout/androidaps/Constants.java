73bb44a4a37386bc14abb323aa621333dadfb528
p5nbTgip0r <cb3f02c5@opayq.com>
2019-10-21 15:22:08 -0700

    Adjust max temptarget values to hard limits

--- app/src/main/java/info/nightscout/androidaps/Constants.java
+++ app/src/main/java/info/nightscout/androidaps/Constants.java
@@ -53,9 +53,9 @@ public class Constants {
     public static final double defaultHypoTTmmol = 6.5d;
 
     public static final double MIN_TT_MGDL = 72d;
-    public static final double MAX_TT_MGDL = 180d;
+    public static final double MAX_TT_MGDL = 270d;
     public static final double MIN_TT_MMOL = 4d;
-    public static final double MAX_TT_MMOL = 10d;
+    public static final double MAX_TT_MMOL = 15d;
 
     //NSClientInternal
     public static final int MAX_LOG_LINES = 100;
